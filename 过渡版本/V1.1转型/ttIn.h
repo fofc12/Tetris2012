d79ff1b2ad56f0c3a50e3aad2a66fe192e9056ce
free2ing <haoyunmail@qq.com>
2016-06-09 14:31:56 +0800

    Release

--- 过渡版本/V1.1转型/ttIn.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TT_IN_H_
-#define _TT_IN_H_
-
-#include"const.h"
-
-int getCmd(int *cmd);
-
-#endif
-
--- 过渡版本/V2.0双人版测试/gameProgress.h
+++
@@ -1,9 +0,0 @@
-#include"const.h"
-
-//游戏的进程
-void gameInit();
-//void showHelp();
-void gameStart(int mode);
-void gameOver(int playerNum);
-void gameExit();
-
